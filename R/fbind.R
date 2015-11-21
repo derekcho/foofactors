62ddb8567be2162ac1442d44b56a56d1f72f61fd
Derek <derek.cho@stat.ubc.ca>
2015-11-20 20:57:59 -0800

    Added files

--- R/fbind.R
+++ R/fbind.R
@@ -1,4 +1,7 @@
-#' Bind two factors together
+#' Bind two factors
+#'
+#' Create a new factor from two existing factors, where the new factor's levels
+#' are the union of the levels of the input factors.
 #'
 #' @param a factor
 #' @param b factor
@@ -6,9 +9,7 @@
 #' @return factor
 #' @export
 #' @examples
-#' fbind(iris$Species[c(1,51,101)], PlantGrowth$group[c(1,11,21)])
-fbind <- function(a,b)
-{
-  factor(c(as.character(a),as.character(b)))
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+fbind <- function(a, b) {
+  factor(c(as.character(a), as.character(b)))
 }
-
